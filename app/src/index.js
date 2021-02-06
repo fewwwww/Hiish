c29e4e192a3332b111dc1ec74733c0786ea44454
Suning Yao <sy2940@nyu.edu>
2021-02-06 22:57:51 +0800

    init

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import HomePage from './containers/HomePage'
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'tachyons';
 
 ReactDOM.render(
-    <HomePage/>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
