4fa4f420dd38e7315142b3b1f2511fc4720ad3cc
Suning Yao <sy2940@nyu.edu>
2021-02-08 22:41:58 +0800

    tailwind is here~

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import './tailwind.output.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="inline-flex">
+        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">
+          Prev
+        </button>
+        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r">
+          Next
+        </button>
+      </div>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
