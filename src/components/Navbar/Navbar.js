dfaca380ff207e4a2db289f3b87be9aa6fc5776c
Suning Yao <sy2940@nyu.edu>
2021-02-10 03:14:06 +0800

    navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,18 +5,20 @@ import search from './search.svg';
 
 function Navbar() {
   return (
-    <div className="bg-pink-50 w-1/6 flex flex-wrap flex-col justify-around">
+    <div className="bg-pink-50 w-1/6 flex flex-wrap flex-col justify-around shadow-lg">
         <div>
-            <img className="mx-auto rounded-3xl shadow-md object-scale-down" src={profile}/>
+            <img className="mx-auto w-48 rounded-3xl shadow-md border-2 border-red-100" src={profile}/>
         </div>
         <div>
-            <img className="mx-auto" src={logo}/>
+            <img className="mx-auto w-64" src={logo}/>
         </div>
-        <div>
-            <div className="flex justify-evenly">
-                <img className="w-auto" src={plus}/>
-                <img className="w-auto" src={search}/>
-              </div>
+        <div className="flex justify-evenly">
+            <div className="mx-auto object-scale-down">
+                <img src={plus}/>
+            </div>
+            <div className="mx-auto object-scale-down">
+                <img src={search}/>
+            </div>
         </div>
     </div>
   );
