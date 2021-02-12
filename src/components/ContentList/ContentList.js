b5f143b89b5e0899a34eb6d2e12d8f49f759e425
Suning Yao <sy2940@nyu.edu>
2021-02-13 02:33:43 +0800

    test

--- src/components/ContentList/ContentList.js
+++ src/components/ContentList/ContentList.js
@@ -11,7 +11,7 @@ function ContentList() {
     const loadFriends = () => {
         const friends = Datas()
         const friendsList = friends.map(friend =>
-            <div key={friend.id} className="friendMessage flex flex-row h-1/6 py-6 border-b border-gray-100">
+            <div key={friend.id} className="friendMessage cursor-pointer flex flex-row h-1/6 py-6 border-b border-gray-100 hover:bg-indigo-50 delay-75 duration-300">
                 <div className="left h-full w-1/12 flex justify-end">
                     <p
                         className="notification bg-red-400 w-auto px-2 flex justify-center rounded-xl text-white font-semibold">{friend.notification}</p>
@@ -19,6 +19,7 @@ function ContentList() {
                 </div>
                 <div className="right w-4/5 flex flex-col justify-around">
                     <div className="top flex flex-row justify-between">
+                        <div></div>
                         <div className="text-3xl font-semibold">{friend.name}</div>
                         <div>{changeTime(friend.time)}</div>
                     </div>
@@ -29,7 +30,7 @@ function ContentList() {
     }
     return (
         <div style={{width: "70%", marginLeft: '20%'}}>
-            <div className="friendRequest flex flex-row h-1/6 py-6 border-b border-gray-100">
+            <div className="friendRequest cursor-pointer flex flex-row h-1/6 py-6 border-b border-gray-100 hover:bg-indigo-50 delay-75 duration-300">
                 <div className="left h-full w-1/12 flex justify-end">
                     <p className="notification bg-red-400 w-auto px-2 flex justify-center rounded-xl text-white font-semibold">1</p>
                     <img className="profile rounded-2xl invisible xl:visible" src={friendRequest}/>
